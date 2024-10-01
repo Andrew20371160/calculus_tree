eb40a33ec1d919bd0ee0eba88217b6a33c158817
Andrew Karam Saber <andrew.k.saber@gmail.com>
2024-10-02 00:52:42 +0300

    Update calculus_tree.h

--- binary_calculus_tree/calculus_tree.h
+++ binary_calculus_tree/calculus_tree.h
@@ -1,8 +1,15 @@
 #ifndef _calculus_tree_h_
 
 #define _calculus_tree_h_
-#include "preprocessor.h"
+#include <queue>
+#include <set>
+#include <list>
+#include <math.h>
+#include <cmath>
+#include <complex>
+#include <fstream>
 
+#include "preprocessor.h"
 #include "node.h"
 
 
@@ -26,7 +33,7 @@
 cout overloading for vector
 */
 template<typename T>
-ostream& operator<<(ostream& stream, const vector<T>& vec) ;
+std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec) ;
 
 
 
@@ -42,7 +49,7 @@ class calculus_tree
         node *root  ;
         /*
         parsing functions
-        note: all parsing functions have 2 inputs a string,and starting index (where to start parsing from)
+        note: all parsing functions have 2 inputs a std::string ,and starting index (where to start parsing from)
         */
 
         /*
@@ -50,28 +57,28 @@ class calculus_tree
         recursively untill the subtree is generated then returns the root of that
         subtree
         */
-        node*parse_parenthese(const string&,unsigned int &start);
+        node*parse_parenthese(const std::string &,unsigned int &start);
         /*
         when finding a function from the keywords
         this function generates a subtree where the root is the function name
         and the child (left child) is the parenthese or expression between them
         */
-        node*parse_function(const string&,unsigned int &start);
+        node*parse_function(const std::string &,unsigned int &start);
         /*
         this function switches between parsing a function or parenthese or just a variable
         then retrun generated tree ( or node in case of variable)
         */
-        node*parse_block(const string &expression,unsigned int &start);
+        node*parse_block(const std::string  &expression,unsigned int &start);
 
         /*
         this function parse the whole expression and generates the mathematical tree
         then returns the root of it
         */
-        node*create_tree(const string &expression,unsigned int &start);
+        node*create_tree(const std::string  &expression,unsigned int &start);
         /*
         returns precedence of the operator at pos in the expression
         */
-        int precedence(const string&expression,unsigned int pos);
+        int precedence(const std::string &expression,unsigned int pos);
         /*
         this is a helper function to create_tree
         based on precedence of the new operator it assigns the place of var
@@ -80,12 +87,12 @@ class calculus_tree
         last_op goes up then exchang it's parent with op
         (of course ret_root is treated to be at max top)
         */
-        void var_op_func(const string&op,node*&var,node*&last_op,node*&ret_root);
+        void var_op_func(const std::string &op,node*&var,node*&last_op,node*&ret_root);
         /*
         this function extracts an operand (or variable no matter the length) or an operator
         and returns it
         */
-        string extract(const string&,unsigned int &start);
+        std::string  extract(const std::string &,unsigned int &start);
 
         //removes src and it's chidren
         //disconnects src from it's parent aswell
@@ -98,7 +105,7 @@ class calculus_tree
             this function extracts an operand till '=' , ',' are met
             (used in evaluation function)
         */
-        string eval_extract(const string&,unsigned int &start);
+        std::string  eval_extract(const std::string &,unsigned int &start);
 
         /*
             the name is obvious
@@ -120,7 +127,7 @@ class calculus_tree
         (if tree contains variables like x) it uses the value where evaluating
         it doesn't replace value of x in the tree
         */
-        DataType evaluate(node*ptr,const list<string>&vars_and_values);
+        DataType evaluate(node*ptr,const  std::list<std::string>& vars_and_values);
 
         /*
         differentiation functions
@@ -130,39 +137,39 @@ class calculus_tree
         switches through functions using fn then returns differentiation of it
         with respect to var
         */
-        string diff_function(const int fn,node*ptr,const string&var );
+        std::string  diff_function(const int fn,node*ptr,const std::string &var );
         /*
             switches through operators to return the differentiation according to the operator
             this function also contains differentiation of '^'
             both cases whether exponent is a constant or an expression are handled
             (accordign to exponent rule)
         */
-        string diff_op(node*ptr,const string&var);
+        std::string  diff_op(node*ptr,const std::string &var);
         /*
         returns left' +    right'
                       -
         */
-        string diff_plus_minus(node*ptr,const string&var);
+        std::string  diff_plus_minus(node*ptr,const std::string &var);
         /*
         applies multiplication rule to ptr's children and returns the differentiation
         */
-        string diff_mult(node*ptr,const string&var);
+        std::string  diff_mult(node*ptr,const std::string &var);
         /*
         applies division rule to ptr's children and returns the differentiation
         */
-        string diff_div(node*ptr,const string&var);
+        std::string  diff_div(node*ptr,const std::string &var);
         /*
         post-traversal while differentiating the tree with respect to var
         */
-        string diff(node*ptr,const string&var);
+        std::string  diff(node*ptr,const std::string &var);
         /*
             if v1 or v2 are zeroes these functions simplify the expression accordingly
             they aslo enclose each expression with parenthese
         */
-        string simplify_add(const string&v1,const string &v2);
-        string simplify_sub(const string&v1,const string &v2);
-        string simplify_mult(const string&v1,const string &v2);
-        string simplify_div(const string&v1,const string &v2);
+        std::string  simplify_add(const std::string &v1,const std::string  &v2);
+        std::string  simplify_sub(const std::string &v1,const std::string  &v2);
+        std::string  simplify_mult(const std::string &v1,const std::string  &v2);
+        std::string  simplify_div(const std::string &v1,const std::string  &v2);
         /*
         returns a copy from src root
         */
@@ -171,13 +178,12 @@ class calculus_tree
             fills ret_set with independent variables in the tree
             (e.g. x,y,v133335353...)
         */
-        void independent_variables_tour(node*ptr,set<string>&ret_set);
-
+        void independent_variables_tour(node*ptr, std::set<std::string>& ret_set);
         bool is_constant_tree(node*ptr)const;
 
         bool is_op_tree(node*ptr)const;
 
-        void save_tour(node* ptr,ofstream&file) const ;
+        void save_tour(node* ptr,std::ofstream&file) const ;
         /*
         is function returns -1 if ptr->symbol isn't a function
         else it returns it's code (the enum value)
@@ -190,7 +196,7 @@ class calculus_tree
         */
         calculus_tree(void);
         //generates the calculust tree form expression
-        calculus_tree(const string&expression);
+        calculus_tree(const std::string &expression);
         //copy constructor
         calculus_tree(const calculus_tree&);
         ~calculus_tree();
@@ -211,7 +217,7 @@ class calculus_tree
             this function sets expression of the tree with expression
             (deletes old one if exists)
         */
-        void set_exp(const string &expression);
+        void set_exp(const std::string  &expression);
 
         /*
             removes whole tree
@@ -221,14 +227,14 @@ class calculus_tree
         this function differeniates the tree with respect to the variable of differentiation
         and returns the resulting tree
         */
-        calculus_tree diff_with(const string&variable);
+        calculus_tree diff_with(const std::string &variable);
         /*
         returns gradient field of the tree
         differentiates the tree with respect to each eantry of ind_vars
         then returns the resulting vector field
         <df/dx1,df/fx2.......etc>
         */
-        vector<calculus_tree<DataType>> gradient(const vector<string>&ind_vars);
+        std::vector<calculus_tree<DataType>> gradient(const std::vector<std::string>&ind_vars);
         /*
         since curl is defined only in 2d or 3d
         this function returns curl of the gradient field (as a vector )
@@ -236,38 +242,38 @@ class calculus_tree
         2nd are the independent variables (you can say dell operator)
 
         */
-        vector<calculus_tree<DataType>> curl(const vector<calculus_tree<DataType>>&gradient_field,
-                                                            const vector<string>&independent_variables);
+        std::vector<calculus_tree<DataType>> curl(const std::vector<calculus_tree<DataType>>&gradient_field,
+                                                            const std::vector<std::string>&independent_variables);
         /*
         this function retruns the divergence of a gradient field (as a calculus tree)
         first input is the gradient_field (can be of any size)
         2nd are the independent variables (must be same size as gradient_field)
         input :(fx,fy,fz),(x,y,z)
         */
-        calculus_tree divergence(const vector<calculus_tree<DataType>>&gradient_field,
-                                                            const vector<string>&independent_variables);
+        calculus_tree divergence(const std::vector<calculus_tree<DataType>>&gradient_field,
+                                                            const std::vector<std::string>&independent_variables);
 
         /*
             first it calculates the gradient with respece to independent_variables
             then differentiates each entry with respect to independent_variables
             fxx+fyy+fzz
         */
-        calculus_tree laplacian(const vector<string>&ind_vars);
+        calculus_tree laplacian(const std::vector<std::string>&ind_vars);
 
         /*
         returns a vector where each entry is a variable in the tree
         */
-        vector<string> independent_variables(void);
+        std::vector<std::string> independent_variables(void);
         /*
         loads an expression and sets current tree to be that expression
         */
-        bool load(const string&filePath);
+        bool load(const std::string &filePath);
         //saves current tree into a file (of the path file_path)
-        bool save(const string&filePath);
+        bool save(const std::string &filePath);
 
         void print(node*ptr = NULL)const;
-        //returns a string of the expression in the current tree
-        string expression(node*ptr = NULL)const;
+        //returns a std::string  of the expression in the current tree
+        std::string  expression(node*ptr = NULL)const;
         /*
         if the tree contains variables like x ,y,c1,v2....
         and you want to evaluate the tree
@@ -275,9 +281,9 @@ class calculus_tree
         if there are no variables then call it as it is
         evaluate_at()
         */
-        DataType evaluate_at(string vars_equal="");
+        DataType evaluate_at(std::string vars_equal="");
         //you can cout a tree
-        friend ostream& operator<<(ostream& os, const calculus_tree& obj) {
+        friend std::ostream& operator<<(std::ostream& os, const calculus_tree& obj) {
 
                 os << obj.expression();
                 return os;
